995c6b509639085c0793b2fd919258f34db77aa9
aivy45 <akshunverma45@gmail.com>
2023-04-11 18:42:57 +0530

    added middleware for logged in

---
+++ Backend/middlewares/auth.middleware.jsx
@@ -0,0 +1,41 @@
+import User from "../Models/user.schema"
+import JWT from "jsonwebtoken"
+import asyncHandler from "../services/asyncHandler"
+import CustomError from "../utilis/custormError"
+import config from "../config/index.js"
+
+
+
+export const isLoggedIn = asyncHandler(async (req, res, next) => {
+    let token;
+    if (req.cookies.token ||
+        (req.headers.authorization && req.headers.authorization.startsWith("Bearer"))
+    ){
+        token = req.cookies.token || req.header.authorization.split("")[1]
+    }
+
+    
+    if(!token)
+    {
+        throw new CustomError('Not authorized to access this route', 401)
+    }
+
+    try{
+    const decodedJwtPayload= JWT.verify(token, config.JWT_SECRET)
+    
+    // grab _id, find user based on id, set this in req.user
+   req.user = await User.findById(decodedJwtPayload._id, "name email role" ) // if name email role not give then give everything
+   next();
+
+}
+    catch(error){
+ throw new CustomError("Not authorized to access this route", 401); 
+    }
+
+
+
+
+})
+
+
+
